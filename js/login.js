9a3ce5b7e0fa4b37b5ced54253524d6629426221
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2024-09-19 00:53:34 +0600

    home page linked

--- js/login.js
+++ js/login.js
@@ -1,9 +1,20 @@
 // Step1: Set Event Handler
 document.getElementById('btn-login').addEventListener('click', function(event){
+
     // Step2: Prevent Default behavior (reloading browser prevents)
     event.preventDefault();
-    console.log('login button clicked');
-    // Step3: Get the phone number
+
+    // Step3: Get the phone number and pin number
     const phoneNumber= document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber= document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    // Step4: Validate phone number and pin
+    if(phoneNumber=== '01751967704' && pinNumber=== '1234'){
+        console.log('You are logged in.');
+        window.location.href = "./home.html";
+    }
+    else{
+        alert('Incorrect Phone Number or PIN')
+    }
 })

e51fbb4d34f407d316de63a6d2663a049cd2015c
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2024-09-19 21:19:46 +0600

    incompleted add money event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('home added');
+// Add money to the account
+
+// Step1: add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    // Prevent page reload
+    event.preventDefault();
+    // Step2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+    // Get the pin number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+})
